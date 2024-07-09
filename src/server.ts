37a3a1352d214dbc6a29a958c2373e5f9f6af0a8
MatDev435 <matheuspsalmazo2@gmail.com>
2024-07-08 21:31:12 -0300

    Setup fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+    return 'Hello World!'
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+    console.log('🔥 HTTP Server Running!')
+})
